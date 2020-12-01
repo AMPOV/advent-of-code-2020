772a861a24d887a5e1dcce6f9c5a506da59176b2
anders <petersson@chillservices.com>
2020-12-01 23:50:44 +0100

    Day1 part 2 added

--- src/main/java/org/ampov/aoc/Day1.java
+++ src/main/java/org/ampov/aoc/Day1.java
@@ -8,21 +8,26 @@ public class Day1 {
 	public static void main(String[] args) {
 		List<Integer> input = new InputFile("input_d1").getAsIntegers();
 		input.sort(Comparator.naturalOrder());
-		System.out.println(getProduct(input));
+		System.out.println("Part1: " + getProduct(input, 0));
+		System.out.println("Part2: " + getProduct(input.subList(1, input.size()), input.get(0)));
 	}
 	
-	private static int getProduct(List<Integer> input) {
-		for (int i = 0; i < input.size(); i++) {
-			int n1 = input.get(i);
-			for (int j = input.size() - 1; j > i; j--) {
-				int n2 = input.get(j);
-				int sum = n1 + n2;
-				if (sum == 2020) 
-					return n1 * n2;
-				else if (sum < 2020) 
-					break;
+	private static int getProduct(List<Integer> input, int factorStart) {
+		int factor1 = input.get(0);
+		for (int i = input.size() - 1; i > 0; i--) {
+			int factor2 = input.get(i);
+			int sum = factorStart + factor1 + factor2;
+			if (sum == 2020) {
+				if (factorStart == 0)
+					return factor1 * factor2;
+				else 
+					return factorStart * factor1 * factor2; 
 			}
+			else if (sum < 2020) 
+				break;
 		}
-		throw new RuntimeException();
+		return getProduct(input.subList(1, input.size()), factor1);
 	}
+	
+
 }
