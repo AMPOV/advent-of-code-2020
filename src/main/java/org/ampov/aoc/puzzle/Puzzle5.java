7a2ca3379bc67ba2bd876d4b2411c1fd32777f61
anders <petersson@chillservices.com>
2020-12-05 13:25:14 +0100

    Simplified

--- src/main/java/org/ampov/aoc/puzzle/Puzzle5.java
+++ src/main/java/org/ampov/aoc/puzzle/Puzzle5.java
@@ -2,22 +2,18 @@ package org.ampov.aoc.puzzle;
 
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.List;
 
 public class Puzzle5 extends AbstractPuzzle {
 	
-	private List<String> input = Arrays.asList(inputFile
-			.getContent()
-			.replaceAll("[FL]", "0")
-			.replaceAll("[BR]", "1")
-			.split("\n"));
-	
 	public Puzzle5() {
-		solutionPart1 = input
-			.stream()
-			.map(seat -> 
-					Byte.parseByte(seat.substring(0, 7), 2) * 8 + 
-					Byte.parseByte(seat.substring(7, 10), 2))
+		solutionPart1 = Arrays.stream(inputFile
+				.getContent()
+				.replaceAll("[FL]", "0")
+				.replaceAll("[BR]", "1")
+				.split("\n"))
+			.map(s -> 
+				Byte.parseByte(s.substring(0, 7), 2) * 8 + 
+				Byte.parseByte(s.substring(7, 10), 2))
 			.max(Comparator.naturalOrder());
 	}
 }
