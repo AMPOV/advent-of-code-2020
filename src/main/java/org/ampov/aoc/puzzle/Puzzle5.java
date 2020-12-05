0a4cf7bb0544260104adaf4e18aa25c287ff0914
anders <petersson@chillservices.com>
2020-12-05 14:50:07 +0100

    Shortened

--- src/main/java/org/ampov/aoc/puzzle/Puzzle5.java
+++ src/main/java/org/ampov/aoc/puzzle/Puzzle5.java
@@ -3,27 +3,27 @@ package org.ampov.aoc.puzzle;
 import java.util.Arrays;
 import java.util.SortedSet;
 import java.util.TreeSet;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 public class Puzzle5 extends AbstractPuzzle {
 	
-	private Function<Stream<Integer>, TreeSet<Integer>> toTree = s -> s.collect(Collectors.toCollection(TreeSet::new));
-	private SortedSet<Integer> seats = toTree.apply(Arrays.stream(inputFile
+	private SortedSet<Integer> seats = Arrays.stream(inputFile
 			.getContent()
 			.replaceAll("[FL]", "0")
 			.replaceAll("[BR]", "1")
 			.split("\n"))
 		.map(s -> 
 			Byte.parseByte(s.substring(0, 7), 2) * 8 + 
-			Byte.parseByte(s.substring(7, 10), 2)
-		));
+			Byte.parseByte(s.substring(7, 10), 2))
+		.collect(Collectors.toCollection(TreeSet::new));
 	
 	public Puzzle5() {
 		solutionPart1 = seats.last();
-		solutionPart2 = toTree.apply(
-				IntStream.range(seats.first(), seats.last()).boxed().filter(i -> !seats.contains(i))).first();
+		solutionPart2 = IntStream.range(seats.first(), seats.last())
+				.boxed()
+				.filter(i -> !seats.contains(i))
+				.collect(Collectors.toCollection(TreeSet::new))
+				.first();
 	}
 }
